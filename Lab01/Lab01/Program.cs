86fffe7811d38c1f4684be6b62ddf80ee76ccd14
Adam Samad <S01229403@acad.tri-c.edu>
2019-10-22 01:30:43 -0400

    Finished with Lab01

---
+++ Lab01/Lab01/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab01
+{
+    print 'hello world from Adam' 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
