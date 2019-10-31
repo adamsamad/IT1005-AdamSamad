6484aeedce2a45a74a46b7033a1fd019507ad652
Adam Samad <S01229403@acad.tri-c.edu>
2019-10-30 23:25:39 -0400

    lab2

--- lab2/lab2/Program.cs
+++ lab2/lab2/Program.cs
@@ -25,3 +25,23 @@ namespace lab2
         } // end Main
     } // end muliplication
 }
+
+/* 1. The C# program applies the execution at the class when you first open it p and then continues to say “hello world”
+
+2. An integer is a number without a decimal point.A float is a floating-point number, which is a number with a decimal place and 
+    used more when a code needs to be more accurate.
+
+3. An example of a block of code method is the ‘GetName’ Method.It is a set code that can give information depending on what you enter, 
+    for example in this GetName method you can return a particular account object name to the caller.
+
+4. ⦁	Like we have types double and int, we can create our own custom types using Classes.  Each class has variables called instance variables or class members.
+To read the value of an instance variable, we create method called a ____ accessor.To assign a value ton instance variable, 
+    we use a method called a ____ accessor.
+
+Get; set 
+
+5. An object is a collection of data and behaviors that represent some entity.A class defines the structure and behaviors of all entities of a given type.
+    The objects are the data that we enter in the code.The classes are the holding places of those objects that keep it together. 
+
+    */
+      
