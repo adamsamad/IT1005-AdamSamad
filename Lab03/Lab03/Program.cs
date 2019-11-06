2e40487765eda59a9be0c6bb9254c81e006ac126
Adam Samad <S01229403@acad.tri-c.edu>
2019-11-06 00:10:28 -0500

    lab03

---
+++ Lab03/Lab03/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace Lab03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int speedLimit = 35;
+            int speed = 42;
+
+            if (speed > speedLimit)
+            {
+                Console.WriteLine("SLOW NOW");
+            }
+
+            else
+            { }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int istrue = 10;
+
+            if (istrue <= 60)
+                Console.WriteLine("It is True");
+
+            else
+                Console.WriteLine("It is False");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input temperature in Fahrenheit: ");
+
+            double fahrenheit = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine();
+
+            double Celsius = (fahrenheit - 32d) * 5d / 9d;
+
+            Console.WriteLine("Temperature in Celsius is {0}: ", Celsius);
+
+            Console.ReadLine();
+
+            if (fahrenheit >= 90) Console.WriteLine("It is hot");
+
+            if (fahrenheit <= 40)
+                Console.WriteLine("It is cold");
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+
+            while (a < 11)
+
+                Console.Write("Value: ");
+
+            Console.WriteLine(a);
+
+            a++;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int b = 60;
+
+            while (b >= 20)
+
+                Console.Write("Value: ");
+
+            Console.WriteLine(b);
+
+            b--;
+        }
+    }
+}
