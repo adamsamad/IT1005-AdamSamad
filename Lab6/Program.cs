826f5bb60218c2af523163846078367063bdc634
Adam Samad <S01229403@acad.tri-c.edu>
2019-11-30 15:09:14 -0500

    Lab6

---
+++ Lab6/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace Lab6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*
+
+            p[ 0 ], p[ 1 ], p[ 2 ], & p[ 3 ]
+
+
+
+
+
+            string[] array = new string[12];
+            Console.WriteLine("{0}{1,15}", "index", "Month");
+            {
+             string[]month = new string[12];
+                month[0] = "January";
+                month[1] = "February";
+                month[2] = "March";
+                month[3] = "April";
+                month[4] = "May";
+                month[5] = "June";
+                month[6] = "July";
+                month[7] = "August";
+                month[8] = "September";
+                month[9] = "October";
+                month[10] = "November";
+                month[11] = "December";
+                for (int i = 0; i < 12; i++)
+                {
+                    Console.WriteLine(month[i]);
+                }
+                
+            }
+
+           
+
+
+            
+
+            string[] seasons = { "Spring", "Summer", "Fall", "Winter" };
+                foreach (string season in seasons)
+                {
+                    Console.WriteLine(season);
+                }
+
+                
+
+
+
+            
+            int[] array = { 101, 142,124,94,524,15 };
+            int total = 0;
+            foreach (int number in array)
+            {
+                total += number;
+            }
+            Console.WriteLine($"Total of array elements: {total}");
+            
+            
+
+
+
+
+            
+            string[] names = { "Al Dente, Anna Graham, Earle Bird, Ginger Rayle, Iona Ford" };
+            
+            int i = 0;
+            foreach (string name in names)
+            {
+                Console.WriteLine("{0,2}. {1}", i, names[i]);
+                i++;
+            }
+            
+            */
+        }
+    }
+}
+
